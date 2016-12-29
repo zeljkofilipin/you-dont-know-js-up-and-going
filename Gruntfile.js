24c115757aca21b78d23bfbb6e6a79836b37d7b6
Željko Filipin <zeljko.filipin@gmail.com>
2016-12-29 12:27:33 +0100

    Set up ESLint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,19 @@
-module.exports = function ( grunt ) {
+module.exports = function (grunt) {
+  require('load-grunt-tasks')(grunt)
 
-	require( 'load-grunt-tasks' )( grunt );
+  // Project configuration
+  grunt.initConfig({
 
-	// Project configuration
-	grunt.initConfig( {
-	} );
+    // Configure ESLint task
+    eslint: {
+      all: [
+        '**/*.js',
+        '!node_modules/**'
+      ]
+    }
 
-	// Default tasks
-	grunt.registerTask( 'default', [] );
+  })
 
-};
+  // Default tasks
+  grunt.registerTask('default', 'eslint')
+}
